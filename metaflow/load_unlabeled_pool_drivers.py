1fcfe4c6bfc002fc09fb749a94866617f5306718
Filippo Buoncompagni <filippo.buoncompagni@multiversecomputing.com>
2023-10-29 16:47:55 +0100

    Separate into smaller functions

--- metaflow/load_unlabeled_pool_drivers.py
+++ metaflow/load_unlabeled_pool_drivers.py
@@ -1,39 +1,47 @@
 import cv2
 import os
 
-def get_im_cv2(path, color_type=1):
+def get_image(path, color_type=1):
     """
-    Load and resize images using the OpenCV library in Python.
+    Load images using the OpenCV library in Python.
 
     Args:
         path (str): The path to the image file.
         color_type (int, optional): The color type of the image. It can be either 1 (grayscale) or 3 (RGB). Default is 1.
 
     Returns:
-        numpy array: The resized image.
+        numpy array: The image.
 
     Example:
         path = 'path/to/image.jpg'
         color_type = 1
-        img = get_im_cv2(path, color_type)
+        image = get_image(path, color_type)
     """
-    # Load as grayscale
+    
     if color_type == 1:
-        img = cv2.imread(path, 0)
+        image = cv2.imread(path, 0)
     elif color_type == 3:
-        img = cv2.imread(path)
+        image = cv2.imread(path)
 
-    #print('Original Dimensions : ',img.shape)
+    return image
 
-    scale_percent = 5 # percent of original size
-    width = int(img.shape[1] * scale_percent / 100)
-    height = int(img.shape[0] * scale_percent / 100)
+def resize_image(image, scale_percent):
+    """
+    Resize an image using the OpenCV library.
+
+    Args:
+        image (numpy array): The input image to be resized.
+        scale_percent (int): The percentage by which the image should be scaled.
+
+    Returns:
+        numpy array: The resized image.
+    """
+    width = int(image.shape[1] * scale_percent / 100)
+    height = int(image.shape[0] * scale_percent / 100)
     dim = (width, height)
-
-    # resize image
-    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    #print('Resized Dimensions : ', resized.shape)
-    return resized
+    resized_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+   
+    return resized_image
 
 def get_unlabeled_pool_drivers(color_type=1):
     """
@@ -47,7 +55,7 @@ def get_unlabeled_pool_drivers(color_type=1):
     """
     X_train = []
     y_train = []
-    
+    SCALE_PERCENT = 5 # percent of original size
     for j in range(10):
         print('Loading data...')
         #print(os.getcwd())
@@ -55,8 +63,9 @@ def get_unlabeled_pool_drivers(color_type=1):
                                 'imgs', 'train', 'c' + str(j))
         
         for file in os.listdir(directory):
-            img = get_im_cv2(os.path.join(directory,file), color_type)
-            X_train.append(img)
+            image = get_image(os.path.join(directory,file), color_type)
+            resized_image = resize_image(image, SCALE_PERCENT)
+            X_train.append(resized_image)
             if j == 0:
                 y_train.append(0)
             else:
